5934c359e890ef679448564a87e14d6b7b84b784
waasimAnsari786 <waasimansari92@gmail.com>
2024-05-21 16:00:41 -0700

    Update index.html
    I created the structure of this project.

---
+++ app.js
@@ -0,0 +1,10 @@
+// let URL = 'https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=0S419hOTpo63JEY1j97i4p3tuSYjIkn5';
+// let URL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=e8b2a8e8dfc6de36a2ca5b889ba4bfa2'
+// let btn = document.querySelector("#btn");
+// const fetchData = async () => {
+//     let response = await fetch(URL2);
+//     let data = await response.json();
+//     console.log(data);
+// };
+
+// btn.addEventListener("click" , fetchData);
